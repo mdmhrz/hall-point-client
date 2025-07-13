1eecc3684796495ffb2eb67cf8c5d01b43841e09
mdmhrz <razufreelance@gmail.com>
2025-07-13 07:01:19 +0600

    JWT implemented

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,42 +3,28 @@ import { useNavigate } from 'react-router';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: `http://localhost:5000`,
     withCredentials: true, // ✅ this sends the HttpOnly cookie
 });
 
 const useAxiosSecure = () => {
-    const { logout } = useAuth(); // Optional: use this if 401/403
+    const { logOut } = useAuth(); // Optional: use this if 401/403
     const navigate = useNavigate();
 
-    // Optional: Response interceptor to handle auth errors
+
     axiosSecure.interceptors.response.use(
         res => res,
-        err => {
-            const status = err?.response?.status;
-            if (status === 401 || status === 403) {
-                logout(); // Optional: clean up local state
-                navigate('/auth/login'); // or show error
+        error => {
+            const status = error.response?.status;
+            if (status === 403) {
+                console.log('Status 403 from second line');
+                navigate('/forbidden');
+            } else if (status === 401) {
+                logOut().then(() => navigate('/auth/login')).catch(console.error);
             }
-            return Promise.reject(err);
+            return Promise.reject(error);
         }
     );
-
-
-
-
-    // axiosSecure.interceptors.response.use(
-    //     res => res,
-    //     error => {
-    //         const status = error.response?.status;
-    //         if (status === 403) {
-    //             navigate('/forbidden');
-    //         } else if (status === 401) {
-    //             logout().then(() => navigate('/login')).catch(console.error);
-    //         }
-    //         return Promise.reject(error);
-    //     }
-    // );
 
     return axiosSecure;
 };
